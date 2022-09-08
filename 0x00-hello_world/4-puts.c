e61471e6d01497365fc45cdf2ab3e0c3de0eb78a
njerikungu <kungumaryanne@gmail.com>
2022-09-08 12:56:06 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Prints exactly as it is
+ * returns 0
+ */
+int main ()
+{
+	char Mystr[]="Programming is like building a multilingual puzzle";
+	puts (Mystr);
+	return 0;
+
+}
