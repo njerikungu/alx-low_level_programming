8f1b60efaad7c04efc18708e18be7e8397dc692e
njerikungu <kungumaryanne@gmail.com>
2022-09-08 15:57:38 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main: will print a string
- * Description- Prints using printf
+ * main: will print using printf
+ * Description- Prints,with proper grammar, but the outcome is a piece of art
  * Return: 0
  */
 
