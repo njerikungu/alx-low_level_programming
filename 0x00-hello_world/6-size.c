8c05712d948be1a17905c733e219fe6fc719cddc
njerikungu <kungumaryanne@gmail.com>
2022-09-08 23:07:35 -0700

    data size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types on the the computer it is compiled and run on
+ *Description - Should produce the exact same output as in the example
+ *Return - 0
+ */
+
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
+}
