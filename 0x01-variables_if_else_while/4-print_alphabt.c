93067d3f90dbb5c95f5f9b373cfd665e60ab2e8d
njerikungu <kungumaryanne@gmail.com>
2022-09-10 11:24:53 -0700

    not all LC

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+*main - prints lowercase alphabets except q and e
+*description - using putchar function
+*Return: 0
+*/
+
+int main(void)
+{
+int ch;
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+if (ch != 'q'; ch != 'e')
+{
+putchar (ch);
+}
+putchar ('\n');
+return (0);
+}
+
