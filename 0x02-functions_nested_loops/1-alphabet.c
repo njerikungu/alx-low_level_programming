d794087a5cbfe97f0ecc71e7a626c6435380340c
njerikungu <kungumaryanne@gmail.com>
2022-09-14 06:18:52 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+*main - prints alphabets in lowercase"
+*Return: (0) as success
+*/
+
+void print_alphabet(void)
+{
+int i
+for (i = 'a'; i = 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
