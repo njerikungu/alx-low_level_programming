f2bf5f99bb2a39b2907f92e32ded6ce7c318807e
njerikungu <kungumaryanne@gmail.com>
2022-09-14 06:30:28 -0700

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 
 /**
 *main - prints alphabets in lowercase"
-*Return: (1) as success
+*Return: (void) as success
 */
 
 void print_alphabet(void)
@@ -14,5 +14,4 @@ for (i = 'a'; i < = 'z'; i++)
 _putchar(i);
 }
 _putchar('\n');
-return (1);
 }
