9c39313e53532e14cbc54c3027cd159815133eba
njerikungu <kungumaryanne@gmail.com>
2022-09-14 14:45:04 -0700

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+*print_last_digit - Entry point
+*@i: function for last digit 
+*description: prints last digit of a number
+*Return: Always 0
+*/
+
+int print_last_	digit(int i)
+{
+int num;
+for (i = num%10)
+{
+_putchar(i);
+}
+return (i);
+}
