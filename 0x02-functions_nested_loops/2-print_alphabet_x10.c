ddbfecdc0d8046a0468e586ba052c8bdcc63e12b
njerikungu <kungumaryanne@gmail.com>
2022-09-14 07:13:12 -0700

    10 lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+*print_alphabet_x10 - prints 10 times the alhabets in lowercase
+*Return void
+*/
+
+void print_alphabet_x10(void)
+{
+char i = 1; s = 'a';
+while (i <= 10)
+{
+while (s <= 'z')
+{
+_putchar(s);
+s++;
+}
+_putchar('\n');
+i++;
+}
+}
