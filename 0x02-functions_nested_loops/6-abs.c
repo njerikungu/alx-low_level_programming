a818441e7a2746b4a66509ea869e8782334d2d6f
njerikungu <kungumaryanne@gmail.com>
2022-09-14 13:56:33 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+*_abs: entry point
+*description: prints absolute value
+*returns: -i if n<0, i if n>=0
+*/
+
+int _abs(int)
+int n;
+{
+if (n < 0)
+{
+return (-i);
+}
+return (i);
+}
