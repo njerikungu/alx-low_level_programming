6fc6113e47c14b41d72b9276e7ae13b41c2869e2
njerikungu <kungumaryanne@gmail.com>
2022-09-14 04:58:12 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,9 +11,9 @@ int main(void)
 {
 char text[8] = "_putchar";
 int i = 0;
-for (i = 0; i <= 8; i++)
+for (i = 0; i <=8; i++)
 {
-_putchar(char[8]);
+_putchar(text[i]);
 }
 _putchar('\n')
 return (0);
