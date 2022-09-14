8f1a46860bec1a21dc9e946f99ebca3923ff874b
njerikungu <kungumaryanne@gmail.com>
2022-09-14 03:11:22 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,12 @@
-Task 0- name of function = '[char]', return type = 'int', argument = "_putchar"
+int _putchar(char c)
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int n);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
