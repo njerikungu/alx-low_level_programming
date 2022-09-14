dbc1c5187b9ae8c9f6c45b34f735459ed59ae123
njerikungu <kungumaryanne@gmail.com>
2022-09-14 02:32:23 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+*main - prints _putchar as a statement
+*description - dont use standard library
+*Return - (0) for success
+*/
+
+int main(void)
+{
+int char[8] = "_putchar";
+int i = 0;
+for (i = 0; i<= 8; i++)
+{
+_putchar(char[8]);
+}
+_putchar('\n')
+return (0);
+}
