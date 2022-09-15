7c46e1a4291ec18ab0274eb29d3440f5dbceb609
njerikungu <kungumaryanne@gmail.com>
2022-09-15 13:17:46 -0700

    print no.s

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,11 @@ i#include "main.h"
 
 void print_numbers(void)
 {
-char i;
-for (i = '0'; i <= '9'; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
-return (0);
+ char i;
+ for (i = '0'; i <= '9'; i++)
+ { 
+  _putchar(i);
+ }
+ _putchar('\n');
+ return (0);
 }
