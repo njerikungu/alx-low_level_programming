ba9e0a805101361d0ef207431cb1a57e719b14a2
njerikungu <kungumaryanne@gmail.com>
2022-09-15 14:18:32 -0700

    more nums

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*more_numbers - prints 10 times the numbers from 0 to 14
+*descrption: only use _putchar 4 times
+*Return: result
+*/
+
+void more_numbers(void)
+{
+char i = 1, j = 0;
+while (i <= 10)
+{
+for (j = '0'; j <= '14'; j++)
+{
+_putchar(j);
+}
+_putchar(i);
+i++
+}
+}
