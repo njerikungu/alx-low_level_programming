73223f5f7af6dd694735195f8023b4fe6ed73198
njerikungu <kungumaryanne@gmail.com>
2022-09-15 12:21:17 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbers - prints numbers from 0 to 9
+*descrption: can only use _putchar twice
+*Return: 1 for success
+*/
+
+void print_numbers(void)
+{
+int i;
+for (i = 0, i <= 9)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
