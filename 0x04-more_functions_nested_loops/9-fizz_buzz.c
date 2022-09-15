76a0d420cb313b58f10bce0e67a249ef79c291e8
njerikungu <kungumaryanne@gmail.com>
2022-09-15 15:45:19 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+*main - prints from 1 to 100
+*Return: 0 for success
+*/
+
+int main(void)
+{
+int i = 1;
+
+while (i <= 100)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%i", i);
+}														
+if (i != 100)
+{															putchar(' ');
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
