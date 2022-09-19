0c225a9fc1fd8c887b4118dc6eaef99e6aa0c2e3
njerikungu <kungumaryanne@gmail.com>
2022-09-19 13:56:22 -0700

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,17 +10,13 @@ void rev_string(char *s)
 {
 int c;
 char rev = s[0];
-int counter = 0;
-
-for (c = 0; s[c] != '\0'; c++)
+int temp = 0;
+while (s[temp] != '\0')
+temp++;
+for (c = 0; c < temp; c++)
 {
-rev[c] = *(s + c);
-}
-c--;
-for (c >= 0; c--)
-{
-*(s + counter) = *(rev + c);
-counter++;
-}
-c++
+temp--;
+rev s[i];
+s[i] = s[temp];
+s[temp] = rev;
 }
