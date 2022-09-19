ceae951e108f6646ecc37254672e916eba4059ac
njerikungu <kungumaryanne@gmail.com>
2022-09-19 14:24:08 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: function for string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+int c, i;
+c = (str);
+for (i = 0; i < c; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
