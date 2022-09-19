d459f52cb04830b5e2e800bdf7c7b81433547d3c
njerikungu <kungumaryanne@gmail.com>
2022-09-19 09:39:02 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*reset_to_98(int *n) - Takes poiner to an int and updates the value
+*description: value it points is 98
+*Return: void
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
