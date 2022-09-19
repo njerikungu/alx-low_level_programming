2fc2027ebc8820fc606253810e877473d57e5282
njerikungu <kungumaryanne@gmail.com>
2022-09-19 16:02:03 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,9 +9,9 @@
 void puts_half(char *str)
 {
 int c;
-for (c = 0; str[c] != '\0'; c++)
+for (c = 0; str[c] != '\0'; c++;)
 c++;
-for (c /= 2; str[c] != '\0')
+for (c /= 2; str[c] != '\0' c++;)
 {
 _putchar(str[c]);
 }
