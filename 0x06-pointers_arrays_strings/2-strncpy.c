8961f0d1672a5ff9e848ea9012c517e4b7bd393b
njerikungu <kungumaryanne@gmail.com>
2022-09-22 13:52:08 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	for (n = 0; src[n] != '\0'; n++)
-	{
-		dest[n] = src[n];
-	}
-	dest[n] = '\0';
-	return (dest);
+	int c = 0;
 
+	for (c = 0; c < n && src[c]; c++)
+	{
+		dest[c] = src[c];
+	}
+	for (c < n; c++)
+	{
+		dest[c] = '\0';
+	}
+	return (dest);
 }
