f472e144763b0b3b56ab897540cf53e4ca0bd356
njerikungu <kungumaryanne@gmail.com>
2022-09-22 04:57:15 -0700

    concatecate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,12 @@
 char *_strcat(char *dest, char *src)
 {
 int i = 0, j = 0;
-while (dest[i] != '\0')
+while (*dest[i] != '\0')
 {
-++dest;
+dest++;
 i++;
 }
-while (src[j] != '\0')
+while (*src[j] != '\0')
 {
 *dest = *src
 dest++;
