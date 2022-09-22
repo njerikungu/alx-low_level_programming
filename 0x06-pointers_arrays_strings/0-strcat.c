d473cb4eefaab6305c9d3093908921536c53f697
njerikungu <kungumaryanne@gmail.com>
2022-09-22 04:47:47 -0700

    concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ **_strcat concatenates two strings
+ *@dest: contains address of string 1
+ *@src: contains address of string 2
+ *Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i = 0, j = 0;
+while (dest [i] != '\0')
+{
+++dest;
+i++;
+}
+while (src [j] != '\0')
+{
+*dest = *src
+dest++;
+src++;
+j++;
+}
+_putchar(dest);
+return (0);
+}
