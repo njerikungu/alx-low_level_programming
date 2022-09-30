a836a8c655cc5c708a5606295e0c9d0a956d02a4
njerikungu <kungumaryanne@gmail.com>
2022-09-30 16:53:42 -0700

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *main - prints mane of program
+ *@argc: arguement count
+ *@argv: array of pointers
+ *Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
