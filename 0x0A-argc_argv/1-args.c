3bcf30f7c25bdafc08940119fd0bdcef1096a072
njerikungu <kungumaryanne@gmail.com>
2022-10-01 02:30:33 -0700

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - prints name of program
+ *@argc: argument count
+ *@argv: argument array
+ *Return: always 0
+ */
+
+int main(int argc, char **argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
