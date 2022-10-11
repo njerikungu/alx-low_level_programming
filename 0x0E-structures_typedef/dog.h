d2cae8971244a9280039baf1d87b08a46bd927f3
njerikungu <kungumaryanne@gmail.com>
2022-10-11 05:57:56 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new data type
+ * @name: member for name
+ * @owner: member for owner
+ * @age: member for age
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	int age;
+};
+
+/**
+ * dog_t - new data type for dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
