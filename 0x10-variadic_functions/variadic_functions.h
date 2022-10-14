0d59a8b06194d7ed267519e39b468f06f9828804
njerikungu <kungumaryanne@gmail.com>
2022-10-14 10:46:43 -0700

    .h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct dt - struct
+ * @letter: data type
+ * @func: pointer
+ */
+typedef struct dt
+{
+	char letter;
+	void (*func)(va_list);
+}print_t;
+
 #endif
