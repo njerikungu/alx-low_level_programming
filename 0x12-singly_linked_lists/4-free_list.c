0ad3e092fd0c0816956d3b83079e18478a5064c3
njerikungu <kungumaryanne@gmail.com>
2022-10-24 11:03:53 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: linked list
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+}
