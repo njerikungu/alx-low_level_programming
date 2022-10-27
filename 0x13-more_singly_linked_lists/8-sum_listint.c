fd36e644f62992d0f165b6238e2876a95a3af911
njerikungu <kungumaryanne@gmail.com>
2022-10-27 12:52:17 -0700

    sum

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_listint - returns the sum of all the data (n) 
+ * sum_listint - returns the sum of all the data (n)
  * @head: pointer to head node
  * Return: 0
  */
